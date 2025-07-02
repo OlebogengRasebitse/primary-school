3d13b8594f38fe809351d944b6c69f6478ad0911
Patience <patiencerasebitse@gmail.com>
2025-07-02 10:48:14 +0200

    Adjusted image height and fixed tepunctuation

--- primary-school/src/components/Hero.jsx
+++ primary-school/src/components/Hero.jsx
@@ -2,11 +2,11 @@ import heroImage from '../assets/hero.jpg';
 
 function Hero() {
   return (
-    <section className="relative h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage:`url(${heroImage})` }}>
+    <section className="relative h-96 flex items-center justify-center bg-cover bg-center" style={{ backgroundImage:`url(${heroImage})` }}>
       <div className="bg-black bg-opacity-50 p-8 rounded-lg text-center text-white">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Tsitsing Primary School— 
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Tsitsing Primary School
 </h1>
-        <p className="text-lg md:text-xl">where young minds grow, explore, and shine! A nurturing environment for learning, creativity, and character-building. .</p>
+        <p className="text-lg md:text-xl">where young minds grow, explore, and shine! A nurturing environment for learning, creativity, and character-building.</p>
       </div>
     </section>
   );
