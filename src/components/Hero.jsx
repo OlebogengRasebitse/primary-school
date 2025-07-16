34767233c87e5fd7fed965f1df39e05e6f6babc8
Patience <patiencerasebitse@gmail.com>
2025-07-16 15:56:10 +0200

    imported images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import image from "../assets/Hero Image.png";
 
 const heroContent = {
   "/": {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import Logo from "../assets/Logo.jpg"; // Adjust the path as necessary
 
 function Navbar() {
   return (
