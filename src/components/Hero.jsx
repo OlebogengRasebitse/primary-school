95766e17028956351a54f65b41dc055ec884a227
Patience <patiencerasebitse@gmail.com>
2025-07-21 17:38:49 +0200

    update image paths for deploy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,28 @@
 import { useLocation } from "react-router-dom";
-import image from "../../public/image.png";
 
 const heroContent = {
   "/": {
-    image: "./public/image.png",
+    image: "/image.png",
     title: "Welcome to Tsitsing Primary School",
     subtitle: "Where young minds grow, explore, and shine! A nurturing environment for learning, creativity, and character-building."
   },
   "/about": {
-    image: "/public/Hero Image 7.png",
+    image: "/Hero Image 7.png",
     title: "About Us",
     subtitle: "Learn more about our mission, vision, and dedicated educators."
   },
   "/contact": {
-    image: "/public/Hero Image 3.png",
+    image: "/Hero Image 3.png",
     title: "Get in Touch",
     subtitle: "We'd love to hear from you! Reach out with your questions or feedback."
   },
   "/events": {
-    image: "/public/Hero Image 5.png",
+    image: "/Hero Image 5.png",
     title: "Events Calendar",
     subtitle: "Stay updated with our upcoming events and important dates."
   },
   "/resources": {
-    image: "/public/Hero Image 4.png",
+    image: "/Hero Image 4.png",
     title: "Resources",
     subtitle: "Access important documents, forms, and educational materials."
   },
@@ -34,7 +33,7 @@ function Hero() {
   const path = location.pathname;
 
   // fallback to home if route not in heroContent
-  const { image, title, subtitle } = heroContent[path] || heroContent["/"];
+  const { image, subtitle } = heroContent[path] || heroContent["/"];
 
  return (
   <section className="relative h-96 w-full overflow-hidden">
