75100dbe605988349e2096ee835bdf31d43f09c1
Patience <patiencerasebitse@gmail.com>
2025-07-27 12:52:42 +0200

    Adjusted hero section to fit mobile view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,14 +36,14 @@ function Hero() {
   const { image, subtitle } = heroContent[path] || heroContent["/"];
 
  return (
-  <section className="relative h-96 w-full overflow-hidden">
+   <section className="relative h-64 sm:h-80 md:h-[60vh] w-full overflow-hidden pt-1">
     <img
       src={image}
       alt="Hero"
       className="absolute top-0 left-0 w-full h-full object-cover object-top"
     />
-    <div className="relative z-10 bg-black bg-opacity-50 p-8 rounded-lg text-center text-white h-full flex items-center justify-center">
-      <h1 className="text-4xl md:text-xl">{subtitle}</h1>
+     <div className="relative z-10 bg-black bg-opacity-50 px-4 py-8 sm:px-6 sm:py-10 md:px-8 md:py-12 flex items-center justify-center h-full text-center">
+        <h3 className="text-base sm:text-2xl md:text-2xl font-semibold text-white">{subtitle}</h3>
       {/* <p className="text-lg md:text-xl">Optional description here</p> */}
     </div>
   </section>
