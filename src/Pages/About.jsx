f7d78a5d9d5e01905840b0b5abb7e2dd5b1870f8
Patience <patiencerasebitse@gmail.com>
2025-07-27 13:05:18 +0200

    Removed br tag and bullet points

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,12 +6,12 @@ function About() {
       <h1 className="text-3xl font-bold mb-4 text-blue-900">MISSION STATEMENT</h1>
       <p className="text-base leading-relaxed">
         At Tsitsing Primary School, our mission is to provide quality, inclusive education that empowers every learner to reach their full potential. We are committed to creating a safe, supportive environment where learning is active, values are taught, and each child is encouraged to grow intellectually, socially, and emotionally.</p>
-      <br /> <br />
+      <br />
       <h1 className="text-3xl font-bold mb-4 text-blue-900">VISION STATEMENT</h1>
       <p className="text-base leading-relaxed">To be a leading public school in Tsitsing Village, known for academic excellence, strong community values, and preparing confident, responsible learners for a changing world </p>
       <br />
           <h1 className="text-2xl font-bold mb-4 text-blue-900">OUR CORE VALUES</h1>
-      <ul className="list-disc list-inside space-y-1">
+      <ul className="space-y-1">
       <li><span className="font-bold">Respect</span> – We treat each other with kindness and dignity.</li>
       <li><span className="font-bold">Integrity</span> – We are honest, responsible, and accountable.</li>
       <li><span className="font-bold">Excellence</span> – We strive for high standards in learning and behavior.</li>
